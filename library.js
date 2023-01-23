2319fa48f03bd5dd2cbecd0e09bda910aba35b87
Matt <matta628@gmail.com>
2023-01-22 21:01:08 -0500

    Hard code multiple examples of books

--- library.js
+++ library.js
@@ -19,10 +19,66 @@ function main() {
   const catch22 = new Book(
     'Catch 22',
     'Joseph Heller',
-    450,
+    453,
     false,
   );
   addBookToLibrary(catch22);
+  const oxygenThief = new Book(
+    'Diary of an Oxygen Thief',
+    'Anonymous',
+    143,
+    true,
+  );
+  addBookToLibrary(oxygenThief);
+  const mathWithoutNums = new Book(
+    'Math Without Numbers',
+    'Milo Beckman',
+    224,
+    true,
+  );
+  addBookToLibrary(mathWithoutNums);
+  const dorianGray = new Book(
+    'The Picture of Dorian Gray',
+    'Oscar Wilde',
+    272,
+    true,
+  );
+  addBookToLibrary(dorianGray);
+  const hitchhikersGuide = new Book(
+    "The Hitchhiker's Guide to the Galaxy #1",
+    'Douglas Adams',
+    193,
+    true,
+  );
+  addBookToLibrary(hitchhikersGuide);
+  const songOfAchilles = new Book(
+    'The Song of Achilles',
+    'Madeline Miller',
+    378,
+    true,
+  );
+  addBookToLibrary(songOfAchilles);
+  const towerOfNero = new Book(
+    'The Tower of Nero',
+    'Rick Riordan',
+    410,
+    true,
+  );
+  addBookToLibrary(towerOfNero);
+  const nineteen84 = new Book(
+    '1984',
+    'George Orwell',
+    328,
+    true,
+  );
+  addBookToLibrary(nineteen84);
+  const oneHundredYears = new Book(
+    'Cien Años de Soledad',
+    'Gabriel García Márquez',
+    417,
+    true,
+  );
+  addBookToLibrary(oneHundredYears);
 }
 
 main();
